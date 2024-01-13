bb5455df2a07018baa784decb86ce8394755c1f2
Roman <roman.smirnov@vicuesoft.com>
2024-01-13 17:25:26 +0400

    fix file name

--- cluster/prefect/prefect-worker/setup.sh
+++ cluster/prefect/prefect-worker/setup.sh
@@ -7,7 +7,7 @@ helm repo add prefect https://prefecthq.github.io/prefect-helm
 helm repo update
 kubectl create namespace prefect
 kubectl apply -f "api-key.yaml"
-helm upgrade --install prefect-worker prefect/prefect-worker -n $PREFECT_K8S_NAMESPACE -f "values2.yaml"
+helm upgrade --install prefect-worker prefect/prefect-worker -n $PREFECT_K8S_NAMESPACE -f "values.yaml"
 
 kubectl create secret docker-registry dockerhub-registry-secret \
     --docker-server="$DOCKERHUB_REGISTRY_SERVER" \
